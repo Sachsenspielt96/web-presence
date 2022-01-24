f7e7450c28c327decdfe9488b727e90e7f31a198
Sachsenspielt96 <sachsenspielt3@gmail.com>
2022-01-24 20:12:27 +0100

    Complete new website with tailwind -> only mobile yet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        background: '#161619',
+        'logo-green': '#00d32d',
+        'logo-darkgreen': '#009b24',
+        'card-background': '#222226',
+      },
+    },
   },
   plugins: [],
 }
