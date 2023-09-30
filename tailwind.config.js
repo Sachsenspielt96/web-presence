b2f7ca5c11d2a250da5458379c4af8a3e7f713ac
Sachsenspielt <sachsenspielt3@gmail.com>
2023-09-30 14:40:49 +0200

    simple logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
   theme: {
-    fontFamily: {
-      sans: ['Outfit', 'sans-serif'],
-    },
     extend: {
       colors: {
         background: '#161619',
         'logo-green': '#00d32d',
         'logo-darkgreen': '#009b24',
         'card-background': '#222226',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+      },
+      animation: {
+        blink: 'blink 1s ease-in-out infinite',
       },
     },
   },
